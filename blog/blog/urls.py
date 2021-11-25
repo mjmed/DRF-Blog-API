493d1e35ae69999862c66b2f99a6727648342225
Medina Maria Jose <mjmedina.c719@gmail.com>
2021-11-25 19:52:39 -0300

    Creando el CRUD de category

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -20,23 +20,26 @@ from rest_framework import permissions
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
+from categories.api.router import router_categories
+
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Blog API",
-      default_version='v1',
-      description="Documentación del Blog API",
-      terms_of_service="",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-#    permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Blog API",
+        default_version='v1',
+        description="Documentación del Blog API",
+        terms_of_service="",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    #    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/', include('users.api.router'))
+    path('api/', include('users.api.router')),
+    path('api/', include(router_categories.urls)),
 ]
