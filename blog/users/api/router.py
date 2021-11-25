41d58730346bf65af1a9fbbae1390164d2774460
Medina Maria Jose <mjmedina.c719@gmail.com>
2021-11-25 18:56:40 -0300

    Obteniendo los datos del usuario logeado

--- blog/users/api/router.py
+++ blog/users/api/router.py
@@ -4,11 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from users.api.views import RegisterView
+from users.api.views import RegisterView, UserView
 
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view()),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/me/', UserView.as_view()),
 ]
--- blog/users/api/views.py
+++ blog/users/api/views.py
@@ -1,10 +1,9 @@
-from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-from users.models import User
-from users.api.serializers import UserRegisterSerializer
+from users.api.serializers import UserRegisterSerializer, UserSerializer
 
 
 # permite registrar un usuario
@@ -17,3 +16,13 @@ class RegisterView(APIView):
             return Response(serializer.data)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# permite obtener los datos del usuario logueado
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+        
