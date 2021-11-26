b67ff0e174e27548b0a430b6ec905288afcc521c
Medina Maria Jose <mjmedina.c719@gmail.com>
2021-11-26 19:42:53 -0300

    Obteniendo los comentarios de un post

--- blog/comments/api/views.py
+++ blog/comments/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from comments.models import Comment
 from comments.api.serializers import CommentSerializer
@@ -10,6 +11,9 @@ class CommentApiViewSet(ModelViewSet):
     queryset = Comment.objects.all()
     
     # ordena el listado de comentarios utilizando el sistema de filtros del rest_framework
-    filter_backends = [OrderingFilter]
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
     ordering = ['-created_at']
+    
+    # filtra los comentarios por el campo post
+    filterset_fields = ['post']
     
