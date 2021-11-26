9a8df979fdccd0740e1be0d4c4c4e3846ef88d81
Medina Maria Jose <mjmedina.c719@gmail.com>
2021-11-26 18:13:25 -0300

    Creando el CRUD de los posts

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -21,6 +21,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
 from categories.api.router import router_categories
+from posts.api.router import router_posts
 
 
 schema_view = get_schema_view(
@@ -42,4 +43,5 @@ urlpatterns = [
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('users.api.router')),
     path('api/', include(router_categories.urls)),
+    path('api/', include(router_posts.urls)),
 ]
---
+++ blog/posts/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.viewsets import ModelViewSet
+
+from posts.models import Post
+from posts.api.serializers import PostSerializer
+
+
+class PostApiViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(published=True)
+    
+    lookup_field = 'slug'
