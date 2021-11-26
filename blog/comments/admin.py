584ef2f22c0f6b28e937f2578be9abb622766738
Medina Maria Jose <mjmedina.c719@gmail.com>
2021-11-26 19:17:00 -0300

    Creando el modelo de los comentarios

--- blog/comments/admin.py
+++ blog/comments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from comments.models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['content', 'user', 'post', 'created_at']
+
--- blog/comments/models.py
+++ blog/comments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models import CASCADE
 
-# Create your models here.
+from users.models import User
+from posts.models import Post
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=CASCADE, null=True)
+    post = models.ForeignKey(Post, on_delete=CASCADE, null=True)
+    
+    def __str__(self):
+        return self.content
