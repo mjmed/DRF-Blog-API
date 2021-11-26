db9e295aa9d862c59719e32f66573c5e68b16be3
Medina Maria Jose <mjmedina.c719@gmail.com>
2021-11-26 18:39:02 -0300

    Obteniendo toda la información del usuario del post

--- blog/posts/api/serializers.py
+++ blog/posts/api/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from posts.models import Post
+from users.api.serializers import UserSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    
     class Meta:
         model = Post
         fields = ['title', 'content', 'slug', 'miniature', 'created_at',
