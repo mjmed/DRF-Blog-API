9907b42302e9c0f37b9c1adb867f0b335c0bb745
Medina Maria Jose <mjmedina.c719@gmail.com>
2021-11-26 18:30:31 -0300

    Añadiendo permisos a los posts

--- blog/posts/api/views.py
+++ blog/posts/api/views.py
@@ -2,9 +2,12 @@ from rest_framework.viewsets import ModelViewSet
 
 from posts.models import Post
 from posts.api.serializers import PostSerializer
+from posts.api.permissions import IsAdminOrReadOnly
 
 
 class PostApiViewSet(ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
+    
     serializer_class = PostSerializer
     queryset = Post.objects.filter(published=True)
     
