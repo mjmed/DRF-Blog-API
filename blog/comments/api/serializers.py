ddf7ef5804aaf78c3426f9833d18821eb50e5270
Medina Maria Jose <mjmedina.c719@gmail.com>
2021-11-26 19:37:13 -0300

    Sistema de orden

--- blog/comments/api/serializers.py
+++ blog/comments/api/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
 from comments.models import Comment
+from users.api.serializers import UserSerializer
+from posts.api.serializers import PostSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    post = PostSerializer()
+    
     class Meta:
         model = Comment
         fields = ['content', 'created_at', 'user', 'post']
--- blog/comments/api/views.py
+++ blog/comments/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter
 
 from comments.models import Comment
 from comments.api.serializers import CommentSerializer
@@ -7,4 +8,8 @@ from comments.api.serializers import CommentSerializer
 class CommentApiViewSet(ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+    
+    # ordena el listado de comentarios utilizando el sistema de filtros del rest_framework
+    filter_backends = [OrderingFilter]
+    ordering = ['-created_at']
     
